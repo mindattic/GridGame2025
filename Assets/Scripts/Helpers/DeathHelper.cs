c054426a0ecf2b9318133e4a0a09cecb8575f340
Ryan DeBraal <ryandebraal@gmail.com>
2025-08-13 08:40:35 -0500

    commit

--- Assets/Scripts/Helpers/DeathHelper.cs
+++ Assets/Scripts/Helpers/DeathHelper.cs
@@ -17,7 +17,7 @@ namespace Assets.Helpers
                 yield break;
 
             // wait until all their HP‐bars are empty
-            yield return new WaitUntil(() => dyingActors.All(x => x.HealthBar.isEmpty));
+            //yield return new WaitUntil(() => dyingActors.All(x => x.HealthBar.isEmpty));
 
             // now actually kill them
             foreach (var actor in dyingActors)
